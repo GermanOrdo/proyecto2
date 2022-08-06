978cc729504f8abc73a37e6af679dd035bc246b5
german <germati91@gmail.com>
2022-08-06 19:43:32 +0200

    proyecto casi final

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
+fetch("./data.json")
+.then(response => response.json())
+.then(destinos => {
+    simulator(destinos);
+})
 
-class Destinos {
-    constructor(name, precio) {
-        this.name = name;
-        this.precio = precio;
-        this.vendido = false;
-    }
-    vender() {
-        this.vendido = true;
-    }
-}
 
-const listaDestinos = [];
-listaDestinos.push(new Destinos("malasia", 1000));
-listaDestinos.push(new Destinos("everest", 2000));
-listaDestinos.push(new Destinos("egipto", 3000));
-listaDestinos.push(new Destinos("niza", 3500));
-listaDestinos.push(new Destinos("malaga", 2500));
-console.log(listaDestinos);
-localStorage.setItem("destinos", JSON.stringify(listaDestinos));
+
+
+
 
--- simu.js
+++ simu.js
@@ -1,31 +1,49 @@
 let destinoElegido;
 let pay;
+function simulator(listaDestinos) {
+    let travelForm = document.getElementById("formulario");
+    travelForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let info = e.target;
+        destinoElegido = (info.children[0].value).toLowerCase();
+        console.log(listaDestinos);
+        const resultado = listaDestinos.find((el) => el.nombre === destinoElegido);
+        console.log(resultado);
+        console.log(listaDestinos);
+        let cuota = info.children[1].value;
+        if (resultado === undefined || resultado === NaN || cuota === "Indique cantidad de cuotas") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Lo siento! algun campo quedo sin rellenar!',
+                background: '#6c757d',
+                timer: 2000,
+            })
+        } else {
+            localStorage.setItem("favoritos", JSON.stringify(resultado));
+            totalPago = resultado.precio * 1.21;
+            pay = (totalPago / cuota).toFixed(2);
+            console.log(pay);
 
-let travelForm = document.getElementById("formulario");
-travelForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let info = e.target;
-    destinoElegido = (info.children[0].value).toLowerCase();
-    const resultado = listaDestinos.find((el) => el.name === destinoElegido);
-    localStorage.setItem("favoritos", JSON.stringify(resultado));
-    let cuota = info.children[1].value;
-    totalPago = resultado.precio * 1.21;
-    pay = (totalPago / cuota).toFixed(2);
-    console.log(pay);
-
-    let consulta = document.getElementById("consulta");
-    let resulDest = document.createElement("div");
-    resulDest.innerHTML = `<h2> Tu destino es ${destinoElegido}</h2>
+            let consulta = document.getElementById("consulta");
+            consulta.innerHTML = "";
+            let resulDest = document.createElement("div");
+            resulDest.innerHTML = `<h2> Tu destino es ${destinoElegido}</h2>
                                 <p> El monto a abonar es $${totalPago} mas impuestos, lo abonas en ${cuota} cuotas de $${pay}. `;
-    consulta.append(resulDest);
-});
+            consulta.append(resulDest);
+        }
+    }
+
+    );
+}
+
 const favDestino = JSON.parse(localStorage.getItem('favoritos')) || [];
 console.log(favDestino);
 
 
 let destFav = document.getElementById("fav1")
 let fav = document.createElement("ul");
-fav.innerHTML = favDestino.name.toUpperCase();
+fav.innerHTML = favDestino.nombre.toUpperCase();
 destFav.append(fav);
 let boton = document.getElementById("boton");
 boton.addEventListener("click", () => {
